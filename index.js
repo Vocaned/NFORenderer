cee0e4cad0ecfcbc1ba183aba1b07d093384e646
Vocaned <10358674+Vocaned@users.noreply.github.com>
2022-03-10 17:04:45 +0200

    Hello World

---
+++ index.js
@@ -0,0 +1,27 @@
+function fileDrop(ev) {
+    ev.preventDefault();
+
+    let file;
+    if (ev.dataTransfer.files) {
+        file = ev.dataTransfer.files[0];
+    } else if (ev.dataTransfer.items) {
+        if (ev.dataTransfer.items[0].kind === 'file') {
+            file = ev.dataTransfer.items[0].getAsFile();
+        }
+    } else return;
+
+    const reader = new FileReader();
+
+    reader.addEventListener("load", () => {
+        let final = [];
+        for (let line of Cp437Helper().convertToUTF8(reader.result).split('\n')) {
+            final.push(line.trimEnd());
+        }
+
+        document.getElementById('content').textContent = final.join("\n");
+    }, false);
+
+    if (file) {
+        reader.readAsArrayBuffer(file);
+    }
+}
